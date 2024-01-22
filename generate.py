4408d22c90a155dff6dd95de91f363453a01a6c6
btnwilson <bentnwilson@gmail.com>
2024-01-22 16:40:13 -0500

    Adjust simulation time

--- generate.py
+++ generate.py
@@ -1,7 +1,28 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = .5
+rows = 0
+while rows < 5:
+    x = rows
+    rows += 1
+    columns = 0
+    while columns < 5:
+        y = columns
+        columns += 1
+        count = 0
+        while count < 10:
+            if .9 ** count == 0:
+                size_scaler = 1
+            else:
+                size_scaler = .9 ** count
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z + (1 * count)], size=[length * size_scaler, width * size_scaler, height * size_scaler])
+            print(count)
+            count += 1
 
 pyrosim.End()
