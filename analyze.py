be691883970e1fc78903876a499031e2a06042c7
btnwilson <bentnwilson@gmail.com>
2024-01-29 15:49:58 -0500

    Add touch sensors and plot output

--- analyze.py
+++ analyze.py
@@ -4,10 +4,11 @@ import numpy as np
 backLegSensorValues = np.load('data/backLegSensorValues.npy')
 frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
 plt.figure()
-plt.plot(backLegSensorValues,label='Back Leg', linewidth=1.5)
-plt.plot(frontLegSensorValues, label='Front Leg')
+plt.plot(backLegSensorValues,label='Back Leg', linewidth=3.5)
+plt.plot(frontLegSensorValues, label='Front Leg', color='red')
 plt.grid()
 plt.legend()
+plt.title('Front and Back leg sensor values')
 plt.plot(frontLegSensorValues)
 
 plt.show()
