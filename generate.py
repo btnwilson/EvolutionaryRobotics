945b380f224c07ec1ecceb95d1b5f9165feabc65
btnwilson <bentnwilson@gmail.com>
2024-01-28 22:57:35 -0500

    Add a robot and try joints

--- generate.py
+++ generate.py
@@ -1,28 +1,24 @@
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = .5
-rows = 0
-while rows < 5:
-    x = rows
-    rows += 1
-    columns = 0
-    while columns < 5:
-        y = columns
-        columns += 1
-        count = 0
-        while count < 10:
-            if .9 ** count == 0:
-                size_scaler = 1
-            else:
-                size_scaler = .9 ** count
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z + (1 * count)], size=[length * size_scaler, width * size_scaler, height * size_scaler])
-            print(count)
-            count += 1
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 0
+    y = 1
+    z = .5
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
 
-pyrosim.End()
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, .5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[1, 0, .5], size=[1, 1, 1])
+    pyrosim.End()
+
+
+create_world()
+create_robot()
