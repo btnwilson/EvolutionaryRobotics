0b45200d55a7be02084d1778ec4c955d2e733be7
btnwilson <bentnwilson@gmail.com>
2024-02-05 15:38:38 -0500

    Make robot walk

--- analyze.py
+++ analyze.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-backLegSensorValues = np.load('data/backLegSensorValues.npy')
-frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+#backLegSensorValues = np.load('data/backLegSensorValues.npy')
+#frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+sin = np.load('data/targetAngles.npy')
 plt.figure()
-plt.plot(backLegSensorValues,label='Back Leg', linewidth=3.5)
-plt.plot(frontLegSensorValues, label='Front Leg', color='red')
+#plt.plot(backLegSensorValues,label='Back Leg', linewidth=3.5)
+#plt.plot(frontLegSensorValues, label='Front Leg', color='red')
+plt.plot(sin)
 plt.grid()
-plt.legend()
+#plt.legend()
 plt.title('Front and Back leg sensor values')
-plt.plot(frontLegSensorValues)
+#plt.plot(frontLegSensorValues)
 
 plt.show()
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,14 +13,26 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-sim_length = 1000
+sim_length = 2500
+amplitudeB = np.pi/8
+frequencyB = 8
+phaseOffsetB = 0
+amplitudeF = np.pi/8
+frequencyF = 8
+phaseOffsetF = np.pi/2
+angleTime = np.linspace(0, 2*np.pi, sim_length)
 backLegSensorValues = np.zeros(sim_length)
 frontLegSensorValues = np.zeros(sim_length)
+targetAngelsB = amplitudeB * np.sin(frequencyB * angleTime + phaseOffsetB)
+targetAngelsF = amplitudeF * np.sin(frequencyF * angleTime + phaseOffsetF)
+#np.save("data/targetAngles.npy", targetAngels)
 for i in range(sim_length):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    time.sleep(1/60)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = targetAngelsB[i], maxForce = 100)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_FrontLeg", controlMode=p.POSITION_CONTROL, targetPosition= targetAngelsF[i], maxForce=100)
+    time.sleep(1/100)
     # print(i)
 p.disconnect()
 
